6877d307e976266eecc8907587c2f3fbf894b5dc
valdemar-signific <valdemar@signific.se>
2026-02-05 11:05:29 +0100

    first commit

---
+++ dags/poc.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+
+def hello():
+    print("Hello from Airflow 🚀")
+
+
+with DAG(
+    dag_id="simple_poc",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+    PythonOperator(
+        task_id="hello_task",
+        python_callable=hello,
+    )
