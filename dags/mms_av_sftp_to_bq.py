a6151fe7d2899726193bd17344df6cea552721b7
valdemar-signific <valdemar@signific.se>
2026-03-11 11:40:37 +0100

    dag to run at 07:00

--- dags/mms_av_sftp_to_bq.py
+++ dags/mms_av_sftp_to_bq.py
@@ -120,7 +120,7 @@ def load_to_bq(**context):
 with DAG(
     dag_id="mms_av_sftp_to_bq",
     start_date=datetime(2026, 3, 8),
-    schedule="@daily",
+    schedule="0 7 * * *",
     catchup=True, # must be true for backfilling and logical date handling!
 ) as dag:
 
